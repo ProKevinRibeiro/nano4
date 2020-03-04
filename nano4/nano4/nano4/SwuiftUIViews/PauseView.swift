5ff0574dff9200c871212393159994ad9fedf1ea
Kevin Ribeiro <kevin.ribeiro@pucpr.edu.br>
2020-03-04 14:47:18 -0300

    SwiftUI views separated in different arquives

---
+++ nano4/nano4/nano4/SwuiftUIViews/PauseView.swift
@@ -0,0 +1,21 @@
+//
+//  PauseView.swift
+//  nano4
+//
+//  Created by Kevin Ribeiro on 04/03/20.
+//  Copyright © 2020 Kevin Ribeiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct PauseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PauseView_Previews: PreviewProvider {
+    static var previews: some View {
+        PauseView()
+    }
+}
---
+++ nano4/nano4/nano4/SwuiftUIViews/SetupView.swift
@@ -0,0 +1,21 @@
+//
+//  SetupView.swift
+//  nano4
+//
+//  Created by Kevin Ribeiro on 04/03/20.
+//  Copyright © 2020 Kevin Ribeiro. All rights reserved.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetupView()
+    }
+}
