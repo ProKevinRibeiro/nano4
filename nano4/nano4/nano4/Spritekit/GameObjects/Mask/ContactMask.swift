bb46ca83a1410d6bd7edf154207fa456ba945608
Kevin Ribeiro <kevin.ribeiro@pucpr.edu.br>
2020-03-05 18:25:05 -0300

    Era pós workshop do Pastre

---
+++ nano4/nano4/nano4/Spritekit/GameObjects/Mask/ContactMask.swift
@@ -0,0 +1,16 @@
+//
+//  ContactMask.swift
+//  SpriteKitWshop
+//
+//  Created by Kevin Ribeiro on 05/03/20.
+//  Copyright © 2020 Bruno Pastre. All rights reserved.
+//
+
+import Foundation
+
+enum ContactMask: UInt8 {
+    case player = 0b00000001
+    case block = 0b00000010
+    case life = 0b00000100
+    }
+
